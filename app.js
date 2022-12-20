8e3d58be48bedd017a90d53fe46c19a20856244f
neeraj <neerajpenumaka7@gmail.com>
2022-12-20 12:36:00 +0530

    understanding get and post

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`App is running on ${port}....`);
+});
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
+});
+
+app.post('/', (req, res) => {
+    res.send('You can now post to this endpoint');
+});
+
