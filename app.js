e31f7f0935e687950481f3dc60f801b6cf116d26
neeraj <neerajpenumaka7@gmail.com>
2022-12-21 10:42:46 +0530

    handling get and post requests

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = 3000;
 
+//middleware
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
+// });
+
+// app.post('/', (req, res) => {
+//     res.send('You can now post to this endpoint');
+// });
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    //console.log(req.body);
+    const newTour = Object.assign({id : newId}, req.body);
+
+ 
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                newTour
+            }
+        });
+    });
+});
+
 app.listen(port, () => {
     console.log(`App is running on ${port}....`);
 });
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
-});
-
-app.post('/', (req, res) => {
-    res.send('You can now post to this endpoint');
-});
 
