5f9407fe7ec569df9272c6452ab2011fb0571e0d
neeraj <neerajpenumaka7@gmail.com>
2023-01-25 12:30:06 +0530

    db connection using mongoose

--- server.js
+++ server.js
@@ -1,10 +1,60 @@
-const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection established!');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR! ', err);
+  });
 
 const app = require('./app');
 
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`App is running on ${port}....`);
+  console.log(`App is running on ${port}....`);
 });
