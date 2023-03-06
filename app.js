5278666134d55a07be7290bf64a20689e61ce230
neeraj <neerajpenumaka7@gmail.com>
2023-03-06 19:42:00 +0530

    error handling

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
 //middleware
 console.log(process.env.NODE_ENV);
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log('Hello from the middleware!');
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
-
-app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-})
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-module.exports = app;
+app.all('*', (req, res, next) => {
+  //   res.status(404).json({
+  //     status: 'fail',
+  //     message: `Can't find ${req.originalUrl} in this server!`,
+  //   });
 
+  next(new AppError(`Can't find ${req.originalUrl} in this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
