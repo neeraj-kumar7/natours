6fcae8c2681ff428d091afd55d4846db1e06c7e2
neeraj <neerajpenumaka7@gmail.com>
2023-03-20 13:34:39 +0530

    completed section 10

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
@@ -10,6 +13,9 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 //middleware
+
+app.use(helmet());
+
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -24,6 +30,9 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.use(express.json());
+
+app.use(mongoSanitize());
+app.use(xss());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
